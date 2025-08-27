d781adf67e2a268858d1b8358a81329305f2899b
ChunkyAtlas <justinsmith4621@proton.me>
2025-08-26 20:14:05 -0600

    - updated README - update build.gradle - update HopperPluginTest

--- src/test/java/com/hopper/HopperPluginTest.java
+++ src/test/java/com/hopper/HopperPluginTest.java
@@ -3,7 +3,7 @@ package com.hopper;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class HopperPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
